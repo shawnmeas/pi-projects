eb1c184222d6a02972eba4927290d44c72f8e2c3
Shawn Meas <shawn@smeas.info>
2022-07-22 10:52:54 -0400

    ledblink: Added SIGINT handling for cleanup on exit.

--- ledblink/src/blink.c
+++ ledblink/src/blink.c
@@ -5,12 +5,31 @@ Author	: Shawn Meas
 
 #include <wiringPi.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 
 //Set pin number for LED
 #define ledPin 0
 
+//Clean exit on SIGINT
+void sigint_handler(int sig_num)
+{
+	printf("\nSIGINT received, exiting...\n");
+
+	//Set all wiringPi pins to low
+	for(int i=0;i<=16;i++)
+	{
+		digitalWrite(i, LOW);
+	}
+
+	exit(0);
+}
+
 int main()
 {
+	//Run clean exit when SIGINT received
+	signal(SIGINT, sigint_handler);
+
 	printf("Running 'blink' program...\n");
 
 	wiringPiSetup();
